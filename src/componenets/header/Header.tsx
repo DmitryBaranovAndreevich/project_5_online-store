00acd029a8caea9e32495ff5fbcd08ee2e06ea24
DmitryBaranovAndreevich <baranow.dmitry@gmail.com>
2023-04-03 20:40:37 +0300

    add: cart page

--- src/componenets/header/Header.tsx
+++ src/componenets/header/Header.tsx
@@ -7,23 +7,46 @@ import loadIcon from '../../assets/loadIcon.png';
 import Input from '../input/Input';
 import womenImg from '../../assets/women.png';
 import Cart from '../cart/Cart';
+import { useMediaQuery } from 'react-responsive';
 
 const Header = () => {
+  const isDesktop = useMediaQuery({
+    query: '(min-width: 1400px)',
+  });
+  const isTablet = useMediaQuery({
+    query: '(min-width: 650px)',
+  });
   return (
-    <header className={styles.container}>
-      <Logo color={'#000'} />
-      <Button text={'Каталог'} size={ButtonSize.big} icon={squaresIcon} onClick={() => {}} />
-      <Input />
-      <div className={styles.contacts}>
-        <div className={styles.info}>
-          <p className={styles.phone}>+7 (777) 490-00-91</p>
-          <p className={styles.time}>время работы: 9:00-20:00</p>
-          <p className={styles.call}>Заказать звонок</p>
+    <header>
+      <div className={styles.container}>
+        <Logo color={'#000'} />
+        {isDesktop && (
+          <>
+            <Button text={'Каталог'} size={ButtonSize.big} icon={squaresIcon} onClick={() => {}} />
+            <Input />
+          </>
+        )}
+        <div className={styles.contacts}>
+          {isTablet && (
+            <div className={styles.info}>
+              <p className={styles.phone}>+7 (777) 490-00-91</p>
+              <p className={styles.time}>время работы: 9:00-20:00</p>
+              <p className={styles.call}>Заказать звонок</p>
+            </div>
+          )}
+          <img src={womenImg} alt="Women" className={styles.image} />
         </div>
-        <img src={womenImg} alt="Women" className={styles.image} />
+        {isDesktop && (
+          <Button text={'Прайс-лист'} size={ButtonSize.big} icon={loadIcon} onClick={() => {}} />
+        )}
+        <Cart />
       </div>
-      <Button text={'Прайс-лист'} size={ButtonSize.big} icon={loadIcon} onClick={() => {}} />
-      <Cart />
+      {!isDesktop && (
+        <div className={styles.container}>
+          <Button text={'Каталог'} size={ButtonSize.big} icon={squaresIcon} onClick={() => {}} />
+          <Input />
+        </div>
+      )}
     </header>
   );
 };
--- src/pages/cart/Cart.tsx
+++ src/pages/cart/Cart.tsx
@@ -1,15 +1,30 @@
+import { useEffect, useState } from 'react';
 import GoodInCart from '../../componenets/goodInCart/GoodInCart';
-import { useAppSelector } from '../../hooks/redux';
+import { useAppDispath, useAppSelector } from '../../hooks/redux';
 import { TGood } from '../../service/goods';
+import { cartSlice } from '../../store/reducers/CartSlice';
+import { Price } from '../goodsCard/GoodsCard';
 import styles from './cart.module.css';
 
 const Cart = () => {
   const { items } = useAppSelector((state) => state.cartReduser);
   const { goods } = useAppSelector((state) => state.goodsReduser);
+  const [isSend, setIsSend] = useState(false);
+  const { clearState } = cartSlice.actions;
+  const dispatch = useAppDispath();
   const goodsArr = items.map((item) => {
     const good = goods.find((good) => good.id === item.id);
     return { item: good, count: item.count };
   }) as Array<{ item: TGood; count: number }>;
+  const sum = goodsArr.reduce((priv, el) => priv + el.count * el.item.price, 0).toFixed(2);
+  const handleClick = () => {
+    dispatch(clearState());
+    setIsSend(true);
+  };
+
+  useEffect(() => {
+    setTimeout(() => setIsSend(false), 3000);
+  }, [isSend]);
   return (
     <div className={styles.container}>
       <h2 className={styles.title}>Корзина</h2>
@@ -18,6 +33,13 @@ const Cart = () => {
       ) : (
         <p className={styles.message}>Пока в корзине товаров нет</p>
       )}
+      {isSend && <p className={styles.messageToSend}>Заказ успешно отправлен</p>}
+      <div className={styles.bottomContainer}>
+        <button className={styles.submitButton} disabled={!goodsArr.length} onClick={handleClick}>
+          Оформить заказ
+        </button>
+        <Price price={Number(sum)} />
+      </div>
     </div>
   );
 };
